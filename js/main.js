73c5408ae5a5979d1e96737ad507c407914e73fb
Tomasz Hencel <you@example.com>
2015-12-20 17:00:20 +0100

    simple logic added

---
+++ js/main.js
@@ -0,0 +1,35 @@
+/**
+ * Created by johny on 20.12.15.
+ */
+var location1 = 3, location2 = 4, location3 = 5;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+while(isSunk == false) {
+    guess = prompt("Ready, aim, fire! (Write number 0-6):");
+    if (guess < 0 || guess > 6) {
+        alert("Please input a valid number!");
+    }
+    else {
+        guesses = guesses + 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert("Hit!");
+            hits = hits + 1;
+
+
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my ship!")
+            }
+        }
+        else {
+            alert("Miss!");
+        }
+    }
+}
+
+var stats = "You needed " + guesses + " hits, to sank my ship, " + "so your efficiency is: " + (3/guesses) + ".";
+alert(stats);
