da9b4cccae9b7e4d85948ad41c0b05680241bb16
Tomasz Hencel <you@example.com>
2016-01-08 18:34:31 +0100

    added declaration of 3 methods in view object

--- js/battleShip.js
+++ js/battleShip.js
@@ -1,3 +1,15 @@
 /**
  * Created by johny on 08.01.16.
  */
+var view = {
+    displayMessage: function (msg) {
+        var messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function (location) {
+
+    },
+    displayMiss: function (location) {
+        
+    }
+};
