68757fabd3193bdde76c96bafff71ef91dd5eb52
Tomasz Hencel <you@example.com>
2016-01-09 15:44:05 +0100

    added logic for game

--- js/battleShip.js
+++ js/battleShip.js
@@ -37,26 +37,76 @@ var model = {
        }
     ],
     fire: function (guess) {
-        var i = 0, ship, locations, index;
-        for (; i < this.numShips; i ++) {
+        var i = 0, ship, index;
+        for (i ; i < this.numShips; i++) {
             ship = this.ships[i];
-            locations = ship.locations;
-            index = locations.indexOf(guess);
+            index = ship.locations.indexOf(guess);
             if (index >= 0) {
                 ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!!");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("You sunk my ship!!");
+                    this.shipsSunk++;
+                }
                 return true;
             }
         }
+        view.displayMiss(guess);
+        view.displayMessage("You missed!!");
         return false;
+    },
+    isSunk: function (ship) {
+        for (var i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") return false;
+        }
     }
 };
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayHit("11");
-view.displayHit("10");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
-view.displayMessage("jojo");
+var controller = {
+    guesses: 0,
+    processGuess: function (guess) {
+        var hit, location = parseGuess(guess);
+        if (location) {
+            this.guesses++;
+            hit = model.fire(location);
+            if (hit && model.shipsSunk === model.numShips) {
+                view.displayMessage("You sunk all my ships in " + this.guesses + " trials!");
+            }
+        }
+    }
+};
+
+function parseGuess(guess) {
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G"], row, firstChar;
+    if (guess === null || guess.length !== 2) {
+      alert("Oops, please enter letter and digital");
+    }
+    else {
+        firstChar = guess.charAt(0);
+        row = alphabet.indexOf(firstChar);
+        column = guess.charAt(1);
+        if (isNaN(row) || isNaN(column)) {
+            alert("Oops, this are not valid coordinates!");
+        }
+        else if (row < 0 || row >= model.boardSize || column < 0 || column >= boardSize) {
+            alert("Oops, your guess is out of board!");
+        }
+        else {
+            return row + column
+        }
+    }
+    return null;
+}
+
+function init() {
+    var fireBtn = document.getElementById("fireButton");
+    fireBtn.onclick = handleFireButton;
+}
+
+function handleFireButton () {
+    var guessInput = document.getElementById("guessInput");
+    var guess = guessInput.value;
+}
+
+window.onload = init;
